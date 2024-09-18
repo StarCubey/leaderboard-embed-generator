f4ae7c2aaa073ed091fdaa6080046469c37ccd0e
StarCubey <SugraCube69@gmail.com>
2024-09-18 15:18:47 -0400

    Let's try this again...

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import solid from "vite-plugin-solid";
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
   plugins: [solid()],
+  base: "/leaderboard-embed-generator/",
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
